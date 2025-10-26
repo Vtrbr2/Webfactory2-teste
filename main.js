5e601c0745c0ad391bb1a0e6e83d3cf12ac373dd
Vtrbr2 <vrodriguespereira691@gmail.com>
2025-10-25 21:54:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -69,28 +69,7 @@ const swiper = new Swiper('.swiper', {
     },
   },
 });
-// ===== ATUALIZAR NAVBAR ATIVA =====
-        window.addEventListener('scroll', () => {
-            const sections = document.querySelectorAll('section');
-            const navLinks = document.querySelectorAll('.nav-link');
-            
-            let currentSection = '';
-            
-            sections.forEach(section => {
-                const sectionTop = section.offsetTop;
-                const sectionHeight = section.clientHeight;
-                if (scrollY >= (sectionTop - 200)) {
-                    currentSection = section.getAttribute('id');
-                }
-            });
-            
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-                if (link.getAttribute('href') === `#${currentSection}`) {
-                    link.classList.add('active');
-                }
-            });
-        });
+
 // Footer current year
 document.getElementById('currentYear').innerText = new Date().getFullYear();
 

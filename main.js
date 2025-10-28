3fab2ebd5ca745d96167478a940671d7d5c617db
Vtrbr2 <vrodriguespereira691@gmail.com>
2025-10-27 22:10:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -120,3 +120,15 @@ const swiper = new Swiper('.logo-carousel', {
                 }
             }
         });
+// ===== EFEITO DE SCROLL (fade-in nos cards e textos) =====
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("visible");
+                }
+            });
+        }, { threshold: 0.2 });
+
+        document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
+
+       
